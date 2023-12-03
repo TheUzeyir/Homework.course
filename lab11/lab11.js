7874b6536827ce841101acfeb79c7d0a6ac0730f
uzeyirmemmedov <memmedovuzeyir797@gmail.com>
2023-12-03 23:07:27 +0400

    develop file

--- lab11/lab11.js
+++ lab11/lab11.js
@@ -6,17 +6,29 @@ let ul=document.querySelector('.message_box_ul')
 
 
 mainInputIcon.addEventListener('click',()=>{
+    if (mainInput.value.trim()==='') {
+        alert('Amil yazi yazmamisan coni');
+
+        return;
+    }
+
+
     let li=document.createElement("li")
-    ul.append(li)
-    li.innerText=mainInput.value.trim()
-
-    let removeBTN=document.createElement('i')
-    removeBTN.classList.add('fa-solid')
-    removeBTN.classList.add('fa-x')
-    ul.appendChild(removeBTN)
-
+    li.textContent=mainInput.value.trim('')
     
-            
+    let removeBTN=document.createElement('i')
+    removeBTN.classList.add("fa-solid")
+    removeBTN.classList.add("fa-times")
+
+    let listBox=document.createElement('div')
+    listBox.appendChild(li)
+    listBox.appendChild(removeBTN)
+    ul.appendChild(listBox)
+    
+    removeBTN.addEventListener('click',()=>{
+        listBox.remove()
+    })
+
+
     mainInput.value=""
 })
-
