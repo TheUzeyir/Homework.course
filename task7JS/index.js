1f81760740f8844271f8330bca9af6880485e832
uzeyirmemmedov <memmedovuzeyir797@gmail.com>
2023-12-04 20:17:00 +0400

    added file

--- task7JS/index.js
+++ task7JS/index.js
@@ -1,7 +1,30 @@
-let password=document.querySelector('.form_box_password-input')
 let btn=document.querySelector('.form_box_button')
 
-password.addEventListener('change',(e)=>{
+btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    let nameInput=document.querySelector('.form_box_name-input')
+    let surNameInput=document.querySelector('.form_box_surname-input')
+    let password=document.querySelector('.form_box_password-input')
     
-    console.log(e.target.value)
+
+
+    let tr=document.createElement('tr')
+    
+    let td1=tr.appendChild(document.createElement('td'));
+    let td2=tr.appendChild(document.createElement('td'))
+
+    td1.textContent=nameInput.value
+    td2.textContent=surNameInput.value
+
+    document.getElementById('tbl').appendChild(tr);
+
+    if (password.value==='') {
+        alert('Please enter password 8 caracter!');
+    }
+
+    nameInput.value=""
+    surNameInput.value=""
+    password.value=""
+
+
 })
