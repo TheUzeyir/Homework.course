8f84f21f23925b2c9aeab3ce730c2e177b948d3e
uzeyirmemmedov <memmedovuzeyir797@gmail.com>
2023-12-08 01:49:46 +0400

    develop file

--- task7JS/index.js
+++ task7JS/index.js
@@ -2,6 +2,7 @@
 let btn=document.querySelector('.form_box_button')
 let nameInput=document.querySelector('.form_box_name-input')
 let surNameInput=document.querySelector('.form_box_surname-input')
+let email=document.querySelector('.form_box_email-input')
 let password=document.querySelector('.form_box_password-input')    
 let tr=document.createElement('tr')
 let td1=tr.appendChild(document.createElement('td'));
@@ -15,18 +16,21 @@ btn.addEventListener('click',(e)=>{
     td1.textContent+=nameInput.value
     td2.textContent+=surNameInput.value
     
-    function Employee(name,surName){
+    function Employee(name,surName,email,password){
         this.name=name
         this.surName=surName
+        this.email=email
+        this.password=password
     }
 
 
-    let person=new Employee(nameInput.value,surNameInput.value)
+    let person=new Employee(nameInput.value,surNameInput.value,email.value,password.value)
     arr.push(person)
-    console.log(person)
+    console.log(arr)
 
     nameInput.value=''
     surNameInput.value=''
+    email.value=''
 
     if (password.value.length<8) {
         alert('Please enter password 8 caracter!');
