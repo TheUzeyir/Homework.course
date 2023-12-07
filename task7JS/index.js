d2d376818ff09f874bede341588994e38ad3f71d
uzeyirmemmedov <memmedovuzeyir797@gmail.com>
2023-12-07 22:34:55 +0400

    develop file

--- task7JS/index.js
+++ task7JS/index.js
@@ -3,30 +3,35 @@ let btn=document.querySelector('.form_box_button')
 let nameInput=document.querySelector('.form_box_name-input')
 let surNameInput=document.querySelector('.form_box_surname-input')
 let password=document.querySelector('.form_box_password-input')    
+let tr=document.createElement('tr')
+let td1=tr.appendChild(document.createElement('td'));
+let td2=tr.appendChild(document.createElement('td'));
+document.getElementById('tbl').appendChild(tr);
 
-btn.addEventListener('submit',()=>{
+let arr=[]
+btn.addEventListener('click',(e)=>{
 
-    let tr=document.createElement('tr')
-    
-    let td1=tr.appendChild(document.createElement('td'));
-    let td2=tr.appendChild(document.createElement('td'))
-
+    e.preventDefault()
     td1.textContent+=nameInput.value
     td2.textContent+=surNameInput.value
-
-    document.getElementById('tbl').appendChild(tr);
-
-
-
-    nameInput.value=""
-    surNameInput.value=""
     
+    function Employee(name,surName){
+        this.name=name
+        this.surName=surName
+    }
+
+
+    let person=new Employee(nameInput.value,surNameInput.value)
+    arr.push(person)
+    console.log(person)
+
+    nameInput.value=''
+    surNameInput.value=''
+
     if (password.value.length<8) {
         alert('Please enter password 8 caracter!');
         td1.style.display="none"
         td2.style.display="none"
     }
     password.value=""
-
-
 })
