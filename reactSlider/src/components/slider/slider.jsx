7f16c1fbafa2788affb06f913c7cbe75d2786df0
uzeyirmemmedov <memmedovuzeyir797@gmail.com>
2024-01-22 16:16:38 +0400

    added file

--- reactSlider/src/components/slider/slider.jsx
+++ reactSlider/src/components/slider/slider.jsx
@@ -8,33 +8,29 @@ import "./slider.css"
 const Slider = ({ data }) => {
   const [counter, setCounter] = useState(0);
 
-  function onIncrementCounter(){
-   if(counter===data.lenght-1){
-    setCounter(0)
-   }
-        setCounter((prev)=>prev+1)
-  }
-
-
-  function onDecrementCounter(){
-    if(counter<=0){
-        setCounter(data.lenght-1)
+  function onIncrementCounter() {
+    if (counter === data.length - 1) {
+      setCounter(0)
     }
-
-        setCounter((prev)=>prev-1)
+    setCounter((prev) => prev + 1)
   }
 
+  function onDecrementCounter() {
+    if (counter <= 0) {
+      setCounter(data.length - 1)
+    }
+    setCounter((prev) => prev - 1)
+  }
 
   return (
     <div className='slider'>
-      <GrChapterPrevious className='slider_btn' onClick={onDecrementCounter}/>
+      <GrChapterPrevious className='slider_btn' onClick={onDecrementCounter} />
       <div className='slider_img'>
         <img src={data[counter]} alt={`image-${counter}`} />
       </div>
-      <GrChapterNext className='slider_btn' onClick={onIncrementCounter}/>
+      <GrChapterNext className='slider_btn' onClick={onIncrementCounter} />
     </div>
   );
 }
 
-export default Slider;
-
+export default Slider; 
--- reactSlider/src/data/sliderData.js
+++ reactSlider/src/data/sliderData.js
@@ -1,5 +1,7 @@
-export const sliderData=[
-    "https://images.unsplash.com/photo-1618042164219-62c820f10723?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dW5zcGFsc2h8ZW58MHx8MHx8fDA%3D",
-    "https://unsplash.com/photos/body-of-water-during-golden-hour-Q6sdzNWsg2o",
-    "https://unsplash.com/photos/a-hand-holding-something-in-a-dark-room-Z_tkPpWxwZ4"
-]
+import img1 from "./neom-0SUho_B0nus-unsplash.jpg"
+import img2 from "./neom-wEgMlQ8DL6s-unsplash.jpg"
+import img3 from "./uran-wang-ao2K-lkq8cI-unsplash.jpg"
+
+
+
+export const sliderData=[img1,img2,img3]
