6c94435b006da60eb8c5305e8face40ab19a806a
uzeyirmemmedov <memmedovuzeyir797@gmail.com>
2024-01-25 20:46:31 +0400

    added file

--- modal/src/companents/modal/Modal.jsx
+++ modal/src/companents/modal/Modal.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ModalBtn from './modalBtn/modalBtn'
 import "./modal.css"
+import { useState } from 'react'
  
 const modal = () => {
 
@@ -15,23 +16,46 @@ const modal = () => {
         Icon.style.display="none"
     }
 
+
+    const [fistNAme, setFirstName] = useState('');
+    const [lastNAme, setLastName] = useState('');
+
+    const giveName = event => {
+      setFirstName(event.target.value);
+      console.log(event.target.value);
+    };
+
+    const giveSurName = event => {
+      setLastName(event.target.value);
+      console.log(event.target.value);
+    };
+
+
   return (
+    <div className="table">
+
+      <h3>First Name:{fistNAme}</h3>
+      <h4>Last Name:{lastNAme}</h4>
+
+    <p className='table_FirstName'></p>
+    <p className='table_LAstName'></p>
+
     <div className='main_modal'>
         <div className="modal">
             <div className="modal_header">
             <h2 className='modal_mainText'>Create your account</h2>
-            <div className="modal_header_btn">
+            <div className="modal_header_btn" onClick={displayNoneModal}>
                 <i className="fa-solid fa-x"></i>
             </div>
             </div>
             <div className="modalCart">
                 <div className="modalCart_box">
                     <div className="modalCart_box_title">First Name</div>
-                    <input className='modalCart_box_input' type="text" placeholder='First name' required />
+                    <input className='modalCart_box_input' type="text" placeholder='First name' required onChange={giveName} value={fistNAme} />
                 </div>
                 <div className="modalCart_box">
                     <div className="modalCart_box_title">Last Name</div>
-                    <input className='modalCart_box_input' type="text" placeholder='Last name' required  />
+                    <input className='modalCart_box_input' type="text" placeholder='Last name' required  onChange={giveSurName} value={lastNAme}/>
                 </div>
             </div>
 
@@ -40,6 +64,7 @@ const modal = () => {
                 <ModalBtn type="simple" btnText="Cancel" handleClick={displayNoneModal}/>
             </div>
         </div>
+    </div>
     </div>
   )
 }
