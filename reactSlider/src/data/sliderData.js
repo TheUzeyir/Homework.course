4bc8216473e6ba2ae430d2052d1b09db545ada58
uzeyirmemmedov <memmedovuzeyir797@gmail.com>
2024-02-03 23:15:26 +0400

    develop file

--- reactSlider/src/data/sliderData.js
+++ reactSlider/src/data/sliderData.js
@@ -1,7 +1,5 @@
-import img1 from "./neom-0SUho_B0nus-unsplash.jpg"
-import img2 from "./neom-wEgMlQ8DL6s-unsplash.jpg"
-import img3 from "./uran-wang-ao2K-lkq8cI-unsplash.jpg"
-
-
+import img1 from "../assets/images/neom-0SUho_B0nus-unsplash.jpg"
+import img2 from "../assets/images/neom-wEgMlQ8DL6s-unsplash.jpg"
+import img3 from "../assets/images/uran-wang-ao2K-lkq8cI-unsplash.jpg"
 
 export const sliderData=[img1,img2,img3]
--- reactSlider/src/pages/home/home.jsx
+++ reactSlider/src/pages/home/home.jsx
@@ -13,5 +13,5 @@ const home = () => {
     </>
   );
 }
-
+ 
 export default home;
