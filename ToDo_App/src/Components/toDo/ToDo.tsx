2ef8a4aa6a6ecbb2566ca13298a8b4f86a71ace7
uzeyirmemmedov <memmedovuzeyir797@gmail.com>
2024-02-15 17:24:18 +0400

    devellop file

--- ToDo_App/src/Components/toDo/ToDo.tsx
+++ ToDo_App/src/Components/toDo/ToDo.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import style from './toDo.module.css';
+import { FaRegCheckCircle } from "react-icons/fa";
 import { MdRadioButtonUnchecked } from 'react-icons/md';
 
-const ToDo = ({ task, toggleComplete, deleteToDo }) => {
+const ToDo = ({ task, toggleComplete }) => {
   return (
     <div className={style.ToDo}>
-      <div onClick={() => toggleComplete(task.id)} className={`${task.completed ? style.completed : style.toDOTask}`}>
-        <MdRadioButtonUnchecked className={style.ToDo_task_icon} onClick={() => deleteToDo(task.id)} />
-        {task.task}
+      <div onClick={() => toggleComplete(task.id)} className={style.toDOTask}>
+        {task.completed ? (
+          <FaRegCheckCircle className={style.ToDo_task_icon_completed}/>
+        ) : (
+          <MdRadioButtonUnchecked className={style.ToDo_task_icon}/>
+        )}
+        <span className={style.taskText + ' ' + (task.completed ? style.completed : '')}>{task.task}</span>
       </div>
     </div>
   );
--- ToDo_App/src/Components/toDoWrapper/ToDoWrapper.tsx
+++ ToDo_App/src/Components/toDoWrapper/ToDoWrapper.tsx
@@ -1,66 +1,62 @@
-import React, { useState } from 'react';
-import ToDoForm from '../toDoForm/ToDoForm';
-import { v4 as uuidv4 } from 'uuid';
-import ToDo from '../toDo/ToDo';
-import style from './toDoWrapper.module.css';
-import lightModeImg from '../../assets/img/lightMode.jpg';
-import { IoMoon } from 'react-icons/io5';
+  import React, { useState } from 'react';
+  import ToDoForm from '../toDoForm/ToDoForm';
+  import { v4 as uuidv4 } from 'uuid';
+  import ToDo from '../toDo/ToDo';
+  import style from './toDoWrapper.module.css';
+  import lightModeImg from '../../assets/img/lightMode.jpg';
+  import { IoMoon } from 'react-icons/io5';
 
-const ToDoWrapper = () => {
-  const [todos, setTodos] = useState([]);
+  const ToDoWrapper = () => {
+    const [todos, setTodos] = useState([]);
 
-  const addTodo = (todo) => {
-    setTodos([
-      ...todos,
-      {
-        id: uuidv4(),
-        task: todo,
-        completed: false,
-        isEditing: false,
-      },
-    ]);
-  };
+    const addTodo = (todo) => {
+      setTodos([
+        ...todos,
+        {
+          id: uuidv4(),
+          task: todo,
+          completed: false,
+          isEditing: false,
+        },
+      ]);
+    };
 
-  const toggleComplete = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
+    const toggleComplete = (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, completed: !todo.completed } : todo
+        )
+      );
+    };
+
+    return (
+      <div className={style.toDoWrapper}>
+        <div className={style.toDoWrapper_imgbox}>
+          <img src={lightModeImg} alt="img" />
+        </div>
+        <div className={style.toDoWrapper_header}>
+          <h2>TODO</h2>
+          <IoMoon />
+        </div>
+        <div className={style.todoform_box}>
+          <ToDoForm addTodo={addTodo} />
+          {todos.map((todo) => (
+            <ToDo task={todo} key={todo.id} toggleComplete={toggleComplete}/>
+          ))}
+          <div className={style.todoform_box_footer}>
+            <span>{todos.filter((todo) => !todo.completed).length} items left</span>
+            <div className={style.todoform_box_footer_status}>
+              <ul>
+                <li>All</li>
+                <li>Active</li>
+                <li>Completed</li>
+              </ul>
+            </div>
+            <p>Clear Completed</p>
+          </div>
+        </div>
+      </div>
     );
   };
 
-  const deleteToDo = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
-
-  return (
-    <div className={style.toDoWrapper}>
-      <div className={style.toDoWrapper_imgbox}>
-        <img src={lightModeImg} alt="img" />
-      </div>
-      <div className={style.toDoWrapper_header}>
-        <h2>TODO</h2>
-        <IoMoon />
-      </div>
-      <div className={style.todoform_box}>
-        <ToDoForm addTodo={addTodo} />
-        {todos.map((todo) => (
-          <ToDo task={todo} key={todo.id} toggleComplete={toggleComplete} deleteToDo={deleteToDo} />
-        ))}
-        <div className={style.todoform_box_footer}>
-          <span>{todos.filter((todo) => !todo.completed).length} items left</span>
-          <div className={style.todoform_box_footer_status}>
-            <ul>
-              <li>All</li>
-              <li>Active</li>
-              <li>Completed</li>
-            </ul>
-          </div>
-          <p>Clear Completed</p>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default ToDoWrapper;
+  export default ToDoWrapper;
